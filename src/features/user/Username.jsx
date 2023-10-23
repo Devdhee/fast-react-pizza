501aa56e93c8a23d27a9f84dba0b968a39a17297
Devdhee <kvngdhavid@gmail.com>
2023-10-23 17:02:28 +0100

    working with global state

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,6 +1,13 @@
+import { useSelector } from 'react-redux';
+
 function Username() {
+  const username = useSelector((state) => state.user.Username);
+
+  if (!username) return null;
   return (
-    <div className='hidden text-sm font-semibold uppercase md:block'>David</div>
+    <div className='hidden text-sm font-semibold uppercase md:block'>
+      {username}
+    </div>
   );
 }
 
